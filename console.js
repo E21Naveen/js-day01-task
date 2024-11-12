486366768537ca5892ed34d0cc0bb5c3c533d1cf
E21Naveen <enugondanaveen@gmail.com>
2024-11-12 10:25:54 +0530

    javascript day01

---
+++ console.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+console.log("TABLE")
+console.table([{name: "naveen", age: 23}, {name: "sai", age: 23}]);
+console.log("GROUP")
+console.group("10k coder","id");
+console.log("naveen 3")
+console.groupEnd()
+console.group()
+console.error("THis is Error Message")
+console.warn("This is warn Message")
+console.groupEnd()
+console.groupCollapsed("this is groupcollapsed");
+console.groupEnd()
+console.info("This is an informational message.in info");
